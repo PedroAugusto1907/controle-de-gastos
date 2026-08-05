a3e82ac890631b1a78f4020ee6d68131e79428ad
Pedro Albuquerque <pedroaugusto19017@gmail.com>
2026-08-05 20:47:29 -0300

    feat: implementa busca por id de pessoas

--- backend/ControleDeGastos/ControleDeGastos/Services/Interfaces/IPessoaService.cs
+++ backend/ControleDeGastos/ControleDeGastos/Services/Interfaces/IPessoaService.cs
@@ -5,6 +5,8 @@ namespace ControleDeGastos.Services.Interfaces;
 public interface IPessoaService {
     Task<IEnumerable<PessoaResponse>> ListarAsync();
 
+    Task<PessoaResponse> ObterPorIdAsync(long id);
+
     Task<PessoaResponse> CriarAsync(CriarPessoaRequest request);
 
     Task DeletarAsync(long id);
